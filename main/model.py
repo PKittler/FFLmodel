a8720f027a94f0f4f7b98dff13435cb81ab2a86d
PGensel <gensel@burando.de>
2023-11-06 21:47:27 +0100

    implemented: - ODE_Y - ODE_Z

--- main/model.py
+++ main/model.py
@@ -54,3 +54,13 @@ def Competitive_Regulation (u, v, k_u, k_v, h, isActivator = True):
         return (((u / k_u) ** h) / (1 + (u / k_u) ** h + (v / k_v) ** h))
     else:
         return (1/ (1 + (u / k_u) ** h + (v / k_v) ** h))
+
+# function for time derivative of Y
+def ODE_Y (t, y, x_active, k_xy, a_y, b_y, beta_y, h, s_x, flag_activator):
+    x = s_x * x_active              # switch for turn X on/off
+    return b_y + beta_y * Regulation(x, k_xy, h, isActivator=flag_activator) - a_y * y
+
+# function for time derivative of Z
+def ODE_Z(t, z, y,  x_active, k_xz, k_yz, a_z, b_z, beta_z, h, s_x, flag_activator):
+    x = s_x * x_active              # switch for turn X on/off
+    return b_z + beta_z * Competitive_Regulation(x, y, k_xz, k_yz, h, isActivator=flag_activator) - a_z * z
