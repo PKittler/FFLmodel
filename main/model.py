8ea6245287437fe73aeecb32b33200ba9f263c75
PGensel <gensel@burando.de>
2023-11-06 20:15:56 +0100

    Initial code

---
+++ main/model.py
@@ -0,0 +1,14 @@
+'''
+file        model.py
+description feed-forward-loop model based upon publication by U. Alon
+date        2023-11-06
+version     0.1
+authors     Max Hirsch, Philipp Kittler
+license     usage, modification and redistribution only for private, educational and scientific purposes, redistribution only without third party dependencies
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+
