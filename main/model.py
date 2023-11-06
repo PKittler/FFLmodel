03f1e985529d48cc5c7abe85bf512c5e2066e3ca
PGensel <gensel@burando.de>
2023-11-06 22:07:13 +0100

    implemented: - Model

--- main/model.py
+++ main/model.py
@@ -61,6 +61,17 @@ def ODE_Y (t, y, x_active, k_xy, a_y, b_y, beta_y, h, s_x, flag_activator):
     return b_y + beta_y * Regulation(x, k_xy, h, isActivator=flag_activator) - a_y * y
 
 # function for time derivative of Z
-def ODE_Z(t, z, y,  x_active, k_xz, k_yz, a_z, b_z, beta_z, h, s_x, flag_activator):
+def ODE_Z (t, z, y,  x_active, k_xz, k_yz, a_z, b_z, beta_z, h, s_x, flag_activator):
     x = s_x * x_active              # switch for turn X on/off
     return b_z + beta_z * Competitive_Regulation(x, y, k_xz, k_yz, h, isActivator=flag_activator) - a_z * z
+
+# function for model to merge ODE_Y and ODE_Z to a ODE system
+def Model (t, init_vars, k_xy, k_xz, k_yz, a_y, b_y, beta_y, a_z, b_z, beta_z, h, s_x, flag_activator):
+    x_active, y, z = init_vars
+
+    dxdt = 0                        # if X is constant, then change rate is 0
+    dydt = ODE_Y(t, y, x_active, k_xy, a_y, b_y, beta_y, h, s_x, flag_activator)
+    dzdt = ODE_Z(t, z, y, x_active, k_xz, k_yz, a_z, b_z, beta_z, h, s_x, flag_activator)
+
+    return [dxdt, dydt, dzdt]
+
